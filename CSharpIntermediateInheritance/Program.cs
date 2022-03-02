762bef0365dff73089ce3da0d9d44fd1aa8d8cfe
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-01 21:08:35 -0600

    fix modifier

--- CSharpIntermediateInheritance/Program.cs
+++ CSharpIntermediateInheritance/Program.cs
@@ -1,14 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
-
-
-
 using System;
 
 namespace CSharpIntermediateInheritance // Note: actual namespace depends on the project name.
 {
-    internal static class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
