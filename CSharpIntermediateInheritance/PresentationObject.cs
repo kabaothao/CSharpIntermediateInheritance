9a641f091f3cd3120442a41c1a1908398f33fd0b
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-01 21:03:27 -0600

    updated codes

--- CSharpIntermediateInheritance/PresentationObject.cs
+++ CSharpIntermediateInheritance/PresentationObject.cs
@@ -6,16 +6,6 @@ using System.Threading.Tasks;
 
 namespace CSharpIntermediateInheritance
 {   
-
-
-    public class Text : PresentationObject //So this is the inheritance notation we just put a colon here and the name of the base class.
-                                           //Note that in C-sharp a class can have only one parent.
-    {
-        public int FontSize { get; set; }
-        public string FontName { get; set; }
-    }
-
-
     internal class PresentationObject
     {
         public int Width { get; set; }  
---
+++ CSharpIntermediateInheritance/Text.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediateInheritance
+{   
+
+    public class Text : PresentationObject //So this is the inheritance notation we just put a colon here and the name of the base class.
+                                           //Note that in C-sharp a class can have only one parent.
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+    }
+}
