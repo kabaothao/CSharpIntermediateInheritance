a6b81fc34c17b1de9e5de78ea38244f7529e70ef
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-01 20:52:22 -0600

    Add project files.

---
+++ CSharpIntermediateInheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+using System;
+
+namespace CSharpIntermediateInheritance // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+/*
+ What is Inheritance?
+A kind of relationship between two classes that allows on to inherit code from the other.
+Is - A
+Example: A Car is a Vehicle.
+ 
+ */
